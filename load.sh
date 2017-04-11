06d566ea5d0d1da82aee56e293a20166da6cd18f
quheng <494862190@qq.com>
2017-04-11 17:51:41 +0800

    remove stale data

--- load.sh
+++
@@ -1,5 +0,0 @@
-dvid repos new TestDataset "Dataset for test" testdataset
-dvid repo test new keyvalue KeyValueStore
-dvid -stdin node test KeyValueStore put testSwc <  ./slice15_L11.swc
-
-# http://127.0.0.1:12345/api/node/testdataset/KeyValueStore/key/testSwc
